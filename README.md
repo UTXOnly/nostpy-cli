b15204118925a8225f6ef5f89e3f4b7590ad533b
Brian Hartford <bhartford419@gmail.com>
2024-05-12 00:14:35 -0400

    Working pip package (#1)

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # nostpy-cli
-Command line nostr client
+
+`nostpy-cli` is a Command Line Interface (CLI) tool designed to handle nostpy events efficiently. This tool provides easy-to-use commands to send and query events via a WebSocket connection.
+
+## Features
+
+- Send events to a specified relay.
+- Query events from a relay.
+
+## Prerequisites
+
+Before installing `nostpy-cli`, ensure you have Python 3.6 or higher installed on your system. You can check your Python version by running:
+
+```
+python3 --version
+```
+## Installation
+### Build from source
+Clone the Repository
+First, clone the repository to your local machine:
+
+```
+git clone https://github.com/UTXOnly/nostpy-cli.git
+cd nostpy-cli
+python3 -m build
+pip install .
+```
+
+### Install Using pip
+To install using `pip` use the command below:
+```
+pip install nostpy-cli
+```
+
+## Usage
+Once installed, you can run nostpy-cli from the command line. Below are some examples of how to use the CLI tool:
+
+### Send Event
+To send an event, you would use a command similar to the following:
+
+```
+nostpy-cli send_event --pubkey "your_public_key" --privkey "your_private_key" --content "your plaintext message" --tags "[['tag1', 'value1']]" --kind 4 --relay "wss://yourrelayurl.com"
+```
+`--pubkey` , `--priv_key` and `--relay` arguments are required, all else are optional
+
+### Query Event
+To query events, use the following command:
+
+```
+nostpy-cli query --kinds "[1,9075]" --relay "wss://yourrelayurl.com"
+```
+`--relay` field required
+### Help
+To view all available commands and their options, use the help command:
+
+```
+nostpy-cli -h
+```
+## Contributing
+Contributions to nostpy-cli are welcome! Please feel free to submit pull requests or open issues to report bugs or suggest enhancements.
+
+### License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ nostpy_cli/create_event.py
@@ -0,0 +1,148 @@
+import asyncio
+import hashlib
+import json
+import secp256k1
+import time
+import websockets
+
+
+class Event:
+    """
+    A class that manages cryptographic events, including their signing, verification, creation, and sending.
+    Also includes querying functionality to request information from WebSocket relays.
+
+    Attributes:
+        relays (list): A list of WebSocket relays to interact with.
+
+    Methods:
+        print_color(text, color):
+            Prints a message in a specified color.
+
+        sign_event_id(event_id, private_key_hex) -> str:
+            Signs an event ID using a private key in hexadecimal format.
+
+        calc_event_id(public_key, created_at, kind_number, tags, content) -> str:
+            Calculates the event ID by hashing event data.
+
+        create_event(public_key, private_key_hex, content, kind, tags) -> dict:
+            Creates an event and returns the structured event data dictionary.
+
+        verify_signature(event_id, pubkey, sig) -> bool:
+            Verifies an event signature against a public key.
+
+        send_event(public_key, private_key_hex, content, kind, tags):
+            Asynchronously sends an event to the configured WebSocket relays.
+
+        query_relays(query_dict, timeout=5):
+            Asynchronously queries the configured WebSocket relays with the specified query parameters.
+    """
+
+    def __init__(self, relays) -> None:
+        self.relays = relays
+
+    def print_color(self, text, color):
+        print(f"\033[1;{color}m{text}\033[0m")
+
+    def sign_event_id(self, event_id: str, private_key_hex: str) -> str:
+        private_key = secp256k1.PrivateKey(bytes.fromhex(private_key_hex))
+        sig = private_key.schnorr_sign(
+            bytes.fromhex(event_id), bip340tag=None, raw=True
+        )
+        return sig.hex()
+
+    def calc_event_id(
+        self,
+        public_key: str,
+        created_at: int,
+        kind_number: int,
+        tags: list,
+        content: str,
+    ) -> str:
+        data = [0, public_key, created_at, kind_number, tags, content]
+        data_str = json.dumps(data, separators=(",", ":"), ensure_ascii=False)
+        return hashlib.sha256(data_str.encode("UTF-8")).hexdigest()
+
+    def create_event(
+        self, public_key: str, private_key_hex: str, content: str, kind: int, tags: list
+    ):
+        created_at = int(time.time())
+        kind
+        event_id = self.calc_event_id(public_key, created_at, kind, tags, content)
+        signature_hex = self.sign_event_id(event_id, private_key_hex)
+        try:
+            self.verify_signature(event_id, public_key, signature_hex)
+        except Exception as exc:
+            print(f"Error verifying sig: {exc}")
+            return
+        event_data = {
+            "id": event_id,
+            "pubkey": public_key,
+            "kind": kind,
+            "created_at": created_at,
+            "tags": tags,
+            "content": content,
+            "sig": signature_hex,
+        }
+        return event_data
+
+    def verify_signature(self, event_id: str, pubkey: str, sig: str) -> bool:
+        try:
+            pub_key = secp256k1.PublicKey(bytes.fromhex("02" + pubkey), True)
+            result = pub_key.schnorr_verify(
+                bytes.fromhex(event_id), bytes.fromhex(sig), None, raw=True
+            )
+            if result:
+                self.print_color(f"Verification successful for event: {event_id}", "32")
+                return True
+            else:
+                self.print_color(f"Verification failed for event: {event_id}", "31")
+                return False
+        except (ValueError, TypeError) as e:
+            print(f"Error verifying signature for event {event_id}: {e}")
+            return False
+
+    async def send_event(self, public_key, private_key_hex, content, kind, tags):
+        try:
+            event_data = self.create_event(
+                public_key, private_key_hex, content, kind, tags
+            )
+            for ws_relay in self.relays_kind4:
+                async with websockets.connect(ws_relay) as ws:
+                    event_json = ("EVENT", event_data)
+                    print("Sending event:")
+                    self.print_color(f"{event_json}", "32")
+                    print(f"to {ws_relay}")
+                    await ws.send(json.dumps(event_json))
+                    response = await asyncio.wait_for(ws.recv(), timeout=10)
+                    print(f"Response from {ws_relay} is : ")
+                    self.print_color(f"{response}", "33")
+        except Exception as exc:
+            print(f"Error in sending event: {exc}")
+
+    async def query_relays(self, query_dict, timeout=5):
+        for relay in self.relays:
+            try:
+                async with websockets.connect(relay) as ws:
+                    query_ws = json.dumps(("REQ", "5326483051590112", query_dict))
+                    await ws.send(query_ws)
+                    print(f"Query sent to relay {relay}: {query_ws}")
+
+                    responses_received = 0
+                    start_time = time.time()
+                    response_limit = query_dict.get("limit", 3)
+
+                    while (
+                        responses_received < response_limit
+                        and (time.time() - start_time) < timeout
+                    ):
+                        try:
+                            response = await asyncio.wait_for(ws.recv(), timeout=1)
+                            self.print_color(f"Response from {relay}: {response}", "32")
+                            responses_received += 1
+                        except asyncio.TimeoutError:
+                            self.print_color(
+                                "No response within 1 second, continuing...", "31"
+                            )
+                            break
+            except Exception as exc:
+                self.print_color(f"Exception is {exc}, error querying {relay}", "31")
